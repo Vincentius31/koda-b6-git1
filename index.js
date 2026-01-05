c9bb8258c21ccc4eae2a7c1c087575a857eae5b0
KODA <vincentius85@gmail.com>
2026-01-05 20:00:31 +0700

    feat: penambahan fitur kalkulasi celcius ke fareheit, reamur, kelvin

---
+++ index.js
@@ -0,0 +1,9 @@
+import { celciusFarenheit } from "./fitur/celciusFarenheit";
+import { celciusKelvin } from "./fitur/celciusKelvin";
+import { celciusReamur } from "./fitur/celciusReamur";
+
+const masukanCelcius = 10;
+
+console.log(celciusFarenheit(masukanCelcius));
+console.log(celciusKelvin(masukanCelcius));
+console.log(celciusReamur(masukanCelcius));
