a9fe6e9d1e28411a024c77664da1e6d63d66fbbe
KODA <vincentius85@gmail.com>
2026-01-05 19:54:17 +0700

    feat: Penambahan fitur konversi dari celcius ke kelvin

---
+++ fitur/celciusKelvin.js
@@ -0,0 +1,12 @@
+export function celciusKelvin(masukanCelcius){
+    if(typeof masukanCelcius !== "number"){
+        throw new Error("Input harus berupa Number!");
+    }
+    try{
+        const res = masukanCelcius + 273.15;
+        return res; 
+    }
+    catch(err){
+        console.log(`Terjadi kesalahan: ${err.message}`);
+    }
+}
