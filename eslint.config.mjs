2a174a70c4cd238916ccbfd2c0b6cff12aa59ad7
KODA <vincentius85@gmail.com>
2026-01-05 20:04:47 +0700

    feat: menambahkan penyesuaian dengan eslint dan package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+
+  {
+    rules: {
+      semi: ["error","always"]
+    }
+
+  }
 ]);
